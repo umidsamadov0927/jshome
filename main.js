4d4aa88bcd49adcab436d6f2e6c0ce8f46920370
umidsamadov0927 <umidsamadov0927@gmail.com>
2025-06-04 15:42:01 +0500

    added

---
+++ main.js
@@ -0,0 +1,103 @@
+// forEach
+// let raqamlar = [5,5,5,5]
+// raqamlar.forEach((item) => {
+//     console.log(item*2)
+// })
+
+// let mevalar = ["olma","anor","nok"]
+// mevalar.forEach(mevalar2=>{
+//     console.log(mevalar2[(0)].toUpperCase() + mevalar2.slice(1))
+// })
+
+// let names = ["umid","sanjar","bobur"]
+// names.forEach(hello => {
+//     console.log("Salom " + hello)
+// })
+
+
+// map
+// let a = [2,4,6,8]
+// let b = a.map(c => c ** 3)
+// console.log(b)
+
+// let narxlar = [10000,20000,30000]
+// let a = narxlar.map(b => b * 1.15)
+// console.log(a)
+
+// let ismlar = ["xurshid","anvar","oybek"]
+// let a = ismlar.map(b => b.toUpperCase())
+// console.log(a)
+
+
+// filter
+// let numbers = [1,2,3,4,5,6,7,8,9,10]
+// let b = numbers.filter(c => c % 2 !== 0)
+// console.log(b)
+
+// let age = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// let b = age.filter(c => c > 18)
+// console.log(b)
+
+
+
+// find
+// let a = [37,193,27,100,200]
+// let b = a.find(c => c > 100)
+// console.log(b)
+
+// let a = [
+//     { ism:"vali"},
+//     { ism:"ikrom"},
+//     { ism:"ali"},
+//     { ism:"sanjar"}
+// ]
+// let b = a.find(c => c.ism === "ali") !== undefined
+// console.log(b)
+
+// let narxlar = [25,100,125,200]
+// let b = narxlar.find(x => x > 100)
+// console.log(b)
+
+
+// includes
+// let bookList = ["kitob","JavaScript"]
+// let b = bookList.includes("JavaScript")
+// console.log(b)
+
+// let mevalar = ["olma","alma","anor","gilos"]
+// let b = mevalar.includes("gilos")
+// console.log(b)
+
+// let raqamlar = prompt("son kiriting")
+// let b = raqamlar.includes(raqamlar)
+// console.log(b)
+
+
+// indexOf
+// let numbers = [4,5,6,7]
+// let b = numbers.indexOf(6)
+// console.log(b)
+
+// let a = ["x","y","z"]
+// let b = a.indexOf("a")
+// console.log(b)
+
+// let numbers = [1,2,3,2,1]
+// let b = numbers.indexOf(2)
+// console.log(b)
+
+
+
+ // sort
+// let a = [3,1,2]
+// let b = a.sort()
+// console.log(b)
+
+// let mevalar = ["olma","banan","anor"]
+// let b = mevalar.sort()
+// console.log(b)
+
+// let numbers = [100,20,3]
+// let b = numbers.sort((a,b) => a - b)
+// console.log(b)
+
